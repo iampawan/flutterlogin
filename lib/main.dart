c6e07b1cc00e65eb5f97087db76062b6e249f833
SHIVANI GUPTA <shivanigupta.stp2015@gmail.com>
2020-07-28 13:31:55 +0530

    issue solved by shivani

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,6 @@ class LoginPageState extends State<LoginPage>
 
   @override
   Widget build(BuildContext context) {
-    
     return new Scaffold(
       backgroundColor: Colors.white,
       body: new Stack(fit: StackFit.expand, children: <Widget>[
@@ -57,48 +56,51 @@ class LoginPageState extends State<LoginPage>
                     new TextStyle(color: Colors.tealAccent, fontSize: 25.0),
               )),
           isMaterialAppTheme: true,
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new FlutterLogo(
-                size: _iconAnimation.value * 140.0,
-              ),
-              new Container(
-                padding: const EdgeInsets.all(40.0),
-                child: new Form(
-                  autovalidate: true,
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                            labelText: "Enter Email", fillColor: Colors.white),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: "Enter Password",
-                        ),
-                        obscureText: true,
-                        keyboardType: TextInputType.text,
-                      ),
-                      new Padding(
-                        padding: const EdgeInsets.only(top: 60.0),
-                      ),
-                      new MaterialButton(
-                        height: 50.0,
-                        minWidth: 150.0,
-                        color: Colors.green,
-                        splashColor: Colors.teal,
-                        textColor: Colors.white,
-                        child: new Icon(FontAwesomeIcons.signInAlt),
-                        onPressed: () {},
-                      )
-                    ],
-                  ),
+          child: SingleChildScrollView(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new FlutterLogo(
+                  size: _iconAnimation.value * 140.0,
                 ),
-              )
-            ],
+                new Container(
+                  padding: const EdgeInsets.all(40.0),
+                  child: new Form(
+                    autovalidate: true,
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                              labelText: "Enter Email",
+                              fillColor: Colors.white),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Enter Password",
+                          ),
+                          obscureText: true,
+                          keyboardType: TextInputType.text,
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 60.0),
+                        ),
+                        new MaterialButton(
+                          height: 50.0,
+                          minWidth: 150.0,
+                          color: Colors.green,
+                          splashColor: Colors.teal,
+                          textColor: Colors.white,
+                          child: new Icon(FontAwesomeIcons.signInAlt),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ]),
